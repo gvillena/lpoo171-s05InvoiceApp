c67b399d0a0088352dd7456464c5a5dbf1b21e64
alumno <alumno@LIMSFLAB04-00D.system-uap.adm>
2017-04-03 20:10:05 -0500

    first commit

---
+++ src/s05invoiceapp/S05InvoiceApp.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s05invoiceapp;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author alumno
+ */
+public class S05InvoiceApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Mensaje de bienvenida al usuario
+        System.out.println("Bienvenido a la Aplicacion Total Factura");
+        System.out.println();
+        
+        // Crear un objeto tipo Scanner con identificador sc
+        Scanner sc = new Scanner(System.in);
+        
+        // Obtener el subtotal del usuario 
+        System.out.println("Ingrese Subtotal:   ");
+        double subtotal = sc.nextDouble();                
+        double impuesto = subtotal * 0.18;
+        double total = subtotal + impuesto;
+        
+        // Mostrar el total de factura 
+        String mensaje = "Total Factura:    " + total + "\n";
+        System.out.println(mensaje);        
+        
+        
+        
+    }
+    
+}
